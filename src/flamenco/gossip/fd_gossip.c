a1c17e32c93f1964c43a5b4f01b6201710677931
Yunhao Zhang <yunzhang@jumptrading.com>
2024-05-22 18:30:39 +0000

    Add test_gossip_echo_vote

--- src/flamenco/gossip/fd_gossip.c
+++ src/flamenco/gossip/fd_gossip.c
@@ -632,8 +632,11 @@ fd_gossip_make_ping( fd_gossip_t * glob, fd_pending_event_arg_t * arg ) {
   fd_sha256_hash( pre_image, FD_PING_PRE_IMAGE_SZ, &ping->token );
 
   /* Sign it */
-
-  (*glob->sign_fun)(glob->sign_arg, ping->signature.uc, pre_image, FD_PING_PRE_IMAGE_SZ);
+  if (glob->my_contact_info.shred_version == 0U) {
+    (*glob->sign_fun)(glob->sign_arg, ping->signature.uc, ping->token.uc, 32);
+  } else {
+    (*glob->sign_fun)(glob->sign_arg, ping->signature.uc, pre_image, FD_PING_PRE_IMAGE_SZ);
+  }
 
   fd_gossip_send( glob, key, &gmsg );
 }
@@ -669,8 +672,11 @@ fd_gossip_handle_ping( fd_gossip_t * glob, const fd_gossip_peer_addr_t * from, f
   fd_sha256_hash( pre_image, FD_PING_PRE_IMAGE_SZ, &pong->token );
 
   /* Sign it */
-  (*glob->sign_fun)(glob->sign_arg, pong->signature.uc, pre_image, FD_PING_PRE_IMAGE_SZ);
-
+  if (glob->my_contact_info.shred_version == 0U) {
+     (*glob->sign_fun)(glob->sign_arg, pong->signature.uc, pong->token.uc, 32);
+   } else {
+     (*glob->sign_fun)(glob->sign_arg, pong->signature.uc, pre_image, FD_PING_PRE_IMAGE_SZ);
+  }
   fd_gossip_send(glob, from, &gmsg);
 }
 
